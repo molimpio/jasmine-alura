bee2c5be325523f30f663435b900b70b18e0b170
moises <moises@inceres.com.br>
2016-12-14 09:06:13 -0200

    add casos de testes aula 1

---
+++ spec/MaiorEMenorSpec.js
@@ -0,0 +1,43 @@
+describe("MaiorEMenor", function() {
+  
+  it("deve entender numeros em ordem nao sequencial", function() {
+    
+    var algoritmo = new MaiorEMenor();
+    algoritmo.encontra([5,15,7,9]);
+    
+    expect(algoritmo.pegaMaior()).toEqual(15);
+    expect(algoritmo.pegaMenor()).toEqual(5);
+    
+  });
+
+  it("deve entender numeros em ordem crescente", function() {
+
+    var algoritmo = new MaiorEMenor();
+    algoritmo.encontra([5,7,8,9]);
+
+    expect(algoritmo.pegaMaior()).toEqual(9);
+    expect(algoritmo.pegaMenor()).toEqual(5);
+
+  });
+
+  it("deve entender numeros em ordem decrescente", function() {
+
+    var algoritmo = new MaiorEMenor();
+    algoritmo.encontra([8,7,6,5]);
+
+    expect(algoritmo.pegaMaior()).toEqual(8);
+    expect(algoritmo.pegaMenor()).toEqual(5);
+
+  });
+
+  it("deve entender lista com um elemento", function() {
+
+    var algoritmo = new MaiorEMenor();
+    algoritmo.encontra([8]);
+
+    expect(algoritmo.pegaMaior()).toEqual(8);
+    expect(algoritmo.pegaMenor()).toEqual(8);
+
+  });
+
+});
