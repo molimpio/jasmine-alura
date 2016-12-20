3fbd6c100d89f77f52d787c4697660fa8055a6ee
moises <moises@inceres.com.br>
2016-12-20 08:41:30 -0200

    organizando com describe aula-3

--- spec/ConsultaSpec.js
+++ spec/ConsultaSpec.js
@@ -7,30 +7,38 @@ describe("Consulta", function() {
     guilherme = new Paciente("Guilherme", 28, 72, 1.80);
   });
 
-  it("nao deve cobrar nada se for retorno", function() {
+  describe("consultas do tipo retorno", function() {
 
-    var consulta = new Consulta(guilherme, [], true, true);
-    expect(consulta.preco()).toEqual(0);
+    it("nao deve cobrar nada se for retorno", function() {
+
+      var consulta = new Consulta(guilherme, [], true, true);
+      expect(consulta.preco()).toEqual(0);
+    });
+
   });
 
-  it("deve cobrar 25 por cada procedimento comum", function() {
+  describe("consultas com procedimentos", function() {
 
-    var procedimentosPadrao = ["proc1", "proc2"];
-    var consulta = new Consulta(guilherme, procedimentosPadrao, false, false);
-    expect(consulta.preco()).toEqual(procedimentosPadrao.length * 25);
-  });
+    it("deve cobrar 25 por cada procedimento comum", function() {
 
-  it("deve cobrar valor pelo procedimento particular", function() {
+      var procedimentosPadrao = ["proc1", "proc2"];
+      var consulta = new Consulta(guilherme, procedimentosPadrao, false, false);
+      expect(consulta.preco()).toEqual(procedimentosPadrao.length * 25);
+    });
 
-    var procedimentosPadrao = ["raio-x", "gesso"];
-    var consulta = new Consulta(guilherme, procedimentosPadrao, false, false);
-    expect(consulta.preco()).toEqual(55 + 32);
-  });
+    it("deve cobrar valor pelo procedimento particular", function() {
 
-  it("deve cobrar preco específico dependendo do procedimento", function() {
+      var procedimentosPadrao = ["raio-x", "gesso"];
+      var consulta = new Consulta(guilherme, procedimentosPadrao, false, false);
+      expect(consulta.preco()).toEqual(55 + 32);
+    });
 
-    var consulta = new Consulta(guilherme, ["procedimento-comum", "raio-x", "procedimento-comum", "gesso"], false, false);
-    expect(consulta.preco()).toEqual(25 + 55 + 25 + 32);
+    it("deve cobrar preco específico dependendo do procedimento", function() {
+
+      var consulta = new Consulta(guilherme, ["procedimento-comum", "raio-x", "procedimento-comum", "gesso"], false, false);
+      expect(consulta.preco()).toEqual(25 + 55 + 25 + 32);
+    });
+
   });
 
 });
